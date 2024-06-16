cf223e10e5574c481e62fa9c2fce7163b346aaaf
nikhil kamble <nikhilkamble19985@gmail.com>
2024-06-16 16:48:10 +0530

    get product using fake api in product.jsxd

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div  style={{display:'flex',
+        justifyContent:"space-around",
+        border:"2px solid red",
+      }}>
+     
+    
+    <div><p>Home</p></div>
+    <div><p>Cart</p></div>
+    </div>
+    </>
+  )
+}
+
+export default Header
